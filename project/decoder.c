35bf5005c31ffe4a7a2190f99ae0a5522b3d9d38
oliver meyer <olivermeyer671@gmail.com>
2025-07-19 16:10:34 -0700

    encoder decoder process explained

--- project/decoder.c
+++ project/decoder.c
@@ -2,17 +2,23 @@
 # DECODER LOGIC
 
 ## PROCEDURE:
-    1. Receive 8-bit compressed codeword in reverse byte order
-    2. Reverse the byte order of the compressed codeword
-    3. Extract Sign, Chord, and Step bits from the compressed codeword
-    4. Use Chord number to determine 
+    1. Receive 8-bit compressed codeword with inverted even bits
+    2. Invert again the even bits of the compressed codeword
+    3. Extract Sign, Chord, and Step bits from the compressed codeword (sign bit: 1 = negative for A-law)
+    4. Use the 3 bits of the Chord number to determine position of the first set bit (no initial set bit in chord 0 for A-law)
+    5. Following the first set bit, the step bits are added to the decompressed codeword.
+    6. Now have 12 magnitude bits, along with a 13th bit that is always zero.
+    7. Left-shift by 3 bits to get the 16 bit decompressed codeword in absolute value.
+    8. If sign bit = 1 (is negative): Invert the bits and add 1.
+    9. This is the decompressed sample.
+    10. Return sample to wav file.
 
 ## INPUT:
-    - 8-Bit compressed codeword in reverse byte order
+    - 8-Bit compressed codeword with even bits inverted
 
 
 ## OUTPUT:
-    - 14-bit decompressed sample 
-    - (but stored in 16-bit integer)
+    - 16-bit decompressed sample 
+    - (With only 13 significant bits)
 
 */
--- project/encoder.c
+++ project/encoder.c
@@ -1,35 +1,44 @@
 /* 
-# ENCODER LOGIC
+# ENCODER LOGIC (A-Law)
 
 ## PROCEDURE:
-    1. Convert sample to it's sign-magnitude representation
+    1. Extract sign, convert 2's complement sample to it's absolute magnitude representation
     2. Find the Chord (count the leading zeroes until most significant bit in sample)
     3. Extract the 4 step bits through masking
-    4. Assemble Sign, Chord, and Step Bits into the compressed codeword
-    5. Invert the codeword bitwise for transmission
+    4. Assemble Sign, Chord, and Step Bits into the compressed codeword (sign bit: 1 = negative for A-law)
+    5. Invert the codeword bitwise (even bits only) for transmission
 
 ## INPUT:
-    - Expects a 14-Bit sample in 2's complement format
+    - Expects a 13-Bit sample in 2's complement format
     - .wav files:
         - Assuming LPCM Format:
             - Linear Pulse Code Modulation
             - 16-bit uncompressed audio samples
             - Sample rate of 44100 Hz
-        - Discard the two least significant bits (through masking?) to get the 14-bit sample
+        - Discard the three least significant bits (through masking?) to get the 13-bit sample
         - Sample will be at minimum an int16_t (signed 16-bit integer defined in c99)
     - Example:
         - 16 bit signed integer input sample in 2's complement (2^16 possible values)
         - Range: (-2^15) -> (+2^15 - 1) = (-32,768 -> 32,767)
-        - Assume 16 bit input of -12,345 in decimal
-        - Convert absolute to binary:                                               0011 0000 0011 1001
-        - Convert to 2's complement (invert bits and add 1):                        1100 1111 1100 0111
-        - Bit-shift two bits right to get 14-bit 2's complement integer:            1111 0011 1111 0001
-        - Convert to sign-magnitude (sign is first bit, invert bits and add 1):     0000 1100 0000 1111
-        - First bit was 1, therefore sign is negative. Now have 13 magnitude bits:     0 1100 0000 1111
+        - Assume 16 bit input of -12,345 in decimal (wav sample input will be in 2's complement binary)
+        - Convert absolute to binary:                                                       0011 0000 0011 1001
+        - Sign is negative, convert to negative 2's complement (invert bits and add 1):     1100 1111 1100 0111
+        - This is the true input to our algorithm from the wav file.
+        - Record first bit (sign bit) from input (1 is negative in 2's complement): sign bit = 1                                                                       
+        - Convert to absolute value (invert bits and add 1):                                0011 0000 0011 1001     -> 0d12345
+        - Shift right by 3 bits to get 13 bit input (13th bit is always zero):              0000 0110 0000 0111
+        - Now have 12 magnitude bits:                                                       ---- 0110 0000 0111
+        - Construct the compressed codeword from the sign bit, the position of the highest set magnitude bit, and four subsequent step bits
+        - Sign bit: 1
+        - Highest set magnitude bit position (12 bits [11,0]): 0110 0000 0111 -> Bit 10 is first set: corresponds to 6 in binary (range [7,0]): bx110
+        - Four subsequent bits after the highest set magnitude bit (bits [9,6]): 10 00
+        - Compressed codeword (sign bit | chord | step bits): 1110 1000
+        - Invert even bits for transmission:                  1011 1101
 
+    
 
 ## OUTPUT:
-    - Returns an 8-Bit compressed codeword in reverse byte order (for transmission?)
+    - Returns an 8-Bit compressed codeword with inverted even bits (for transmission?)
     - At minimum will return an int8_t (signed 8-bit integer defined in c99)
 
 
@@ -40,9 +49,9 @@
 
 int signum ( int sample ) {
     if( sample < 0)
-        return( 0) ; /* sign is ’0’ for negative samples */
+        return(1) ; /* sign is ’1’ for negative samples */
     else:
-        return( 1) ; /* sign is ’1’ for positive samples */
+        return(0) ; /* sign is ’0’ for positive samples */
 }
 int magnitude ( int sample ) {
     if( sample < 0) {
